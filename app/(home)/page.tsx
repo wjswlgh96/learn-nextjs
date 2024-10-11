03c8ca508b6eb8daa9e1737660a6ee02a1e0013d
JiHo Jeon <wjswlgh96@naver.com>
2024-10-11 16:55:53 +0900

    data fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,18 @@
-export const metadata = {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
   title: "Home",
 };
 
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
